a6492bf4203eb69d1e688e3d09f401f72901639d
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2024-04-22 11:09:55 -0300

    Estrutura do projeto

---
+++ src/main/java/edvaldooliveira/projetojavaspring/ProjetoJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package edvaldooliveira.projetojavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoJavaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edvaldooliveira/projetojavaspring/ProjetoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edvaldooliveira.projetojavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
